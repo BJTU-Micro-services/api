61bc9f0541d24bf31916e89ce9b2bd628a7b592e
Florian Glorioz <florian.glorioz@gmail.com>
2020-04-18 01:28:44 +0200

    Init project

---
+++ main.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const request = require("request");
+
+app.get("/", (req, res) => res.send("Hello World from API!"));
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
+
+setInterval(() => {
+  request("http://payment:8080", { json: true }, (err, res, body) => {
+    console.log(err ? err : body);
+  });
+  request("http://ticket:8081", { json: true }, (err, res, body) => {
+    console.log(err ? err : body);
+  });
+  request("http://user:8082", { json: true }, (err, res, body) => {
+    console.log(err ? err : body);
+  });
+}, 2000);
